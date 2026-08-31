29ef2716b0d7d8dc2f4d13d94d872cc8fe8ee6e3
Малахов Егор <45966160+BekoSa@users.noreply.github.com>
2026-09-01 01:59:22 +0700

    Route client messages through assigned peer identity

--- src/GoingCooperative.Plugin.BepInEx/Replication/ReplicationPrioritisedObjectWorkV1.cs
+++ src/GoingCooperative.Plugin.BepInEx/Replication/ReplicationPrioritisedObjectWorkV1.cs
@@ -141,7 +141,7 @@ namespace GoingCooperative.Plugin.BepInEx
             }
 
             var command = new LockstepCommand(
-                ReplicationClientPeerId,
+                GetReplicationLocalPeerId(),
                 ++replicationIntentSequence,
                 0L,
                 CommandKind.Custom,
